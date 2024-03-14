65270b46ef572b48ff481d439c2c41a0350e074c
w2xi <43wangxi@gmail.com>
2024-03-14 19:27:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Repl } from '@vue/repl'
+import Monaco from '@vue/repl/monaco-editor'
+import Header from './Header.vue'
+</script>
+
+<template>
+  <Header />
+  <Repl :editor="Monaco" />
+</template>
---
+++ src/Header.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1>Vue 3 REPL</h1>
+  </header>
+</template>
